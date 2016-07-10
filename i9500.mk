824984c8cb8e7d09f01eabe8541d1cd3d93d7234
Alberto96 <albertoguanti@gmail.com>
2016-07-10 12:18:55 +0200

    i9500: Enable radio.apm_sim_not_pwdn

--- i9500.mk
+++ i9500.mk
@@ -208,7 +208,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.data.large_tcp_window_size=true \
-    ro.telephony.ril_class=ExynosXMM6360RIL
+    ro.telephony.ril_class=ExynosXMM6360RIL \
+    persist.radio.apm_sim_not_pwdn=1
 
 # Tethering Config
 PRODUCT_PROPERTY_OVERRIDES += \
