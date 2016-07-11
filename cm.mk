450be23ecfa7c314751c4a53cbfdcc51911d1a0b
Alberto96 <albertoguanti@gmail.com>
2016-07-11 14:51:08 +0200

    Convert into CM-Type tree

--- cm.mk
+++ cm.mk
@@ -1,35 +1,10 @@
-# Copyright (C) 2015 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit from i9500 device
-$(call inherit-product, device/samsung/i9500/i9500.mk)
+$(call inherit-product, device/samsung/i9500/full_i9500.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-PRODUCT_NAME := cm_i9500
-PRODUCT_DEVICE := i9500
-PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := GT-I9500
-
-PRODUCT_BRAND := Samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=GT-I9500 \
@@ -39,4 +14,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="samsung/ja3gxx/ja3g:5.0.1/LRX22C/I9500XXUHOL1:user/release-keys" \
     PRIVATE_BUILD_DESC="ja3gxx-user 5.0.1 LRX22C I9500XXUHOL1 release-keys"
 
-TARGET_UNOFFICIAL_BUILD_ID := GearCM
--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-add_lunch_combo cm_i9500-user
-add_lunch_combo cm_i9500-userdebug
-add_lunch_combo cm_i9500-eng
