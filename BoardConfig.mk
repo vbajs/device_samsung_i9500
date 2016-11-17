3273b921f829d6b732e637f31b5602898f487e89
Alberto96 <albertoguanti@gmail.com>
2016-11-17 21:47:18 +0100

    Revert "i9500: BoardConfig: slim-down TWRP [2]"
    (reverted from commit 2788411efcfebccaedcf9c1a38bb5fcd8c5080d6)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -181,7 +181,7 @@ TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_INCLUDE_L_CRYPTO := true
 TW_MAX_BRIGHTNESS := 255
 TW_NO_EXFAT_FUSE := true
-TW_THEME := portrait_mdpi
+TW_THEME := portrait_hdpi
 
 # WiFi
 BOARD_HAVE_SAMSUNG_WIFI          := true
