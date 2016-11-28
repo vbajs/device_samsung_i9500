ebf567bfbaab27af58cc80480974ef5cc85d0166
dragonGR <alextsanisbadlorg@gmail.com>
2016-11-28 18:46:35 +0000

    i9500: Fix RIL outgoing call

--- include/telephony/ril.h
+++ include/telephony/ril.h
@@ -304,6 +304,7 @@ typedef struct {
     char *          name;       /* Remote party name */
     int             namePresentation; /* 0=Allowed, 1=Restricted, 2=Not Specified/Unknown 3=Payphone */
     RIL_UUS_Info *  uusInfo;    /* NULL or Pointer to User-User Signaling Information */
+    int reserved;
 } RIL_Call;
 
 /* Deprecated, use RIL_Data_Call_Response_v6 */
