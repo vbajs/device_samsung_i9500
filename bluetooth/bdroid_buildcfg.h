cecd59f0684e50ac38d55d6eca119c5bb515f150
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-12-10 21:56:43 +0000

    i9500: Our kernel is missing CLOCK_BOOTTIME_ALARM (alarmtimer)
    Change-Id: Ia3a33ce5420e0b9b3a3e0474ce7c4809e05064f0
    
    Conflicts:
    	bluetooth/bdroid_buildcfg.h

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,6 +18,10 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME   "Galaxy S4"
+
+/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
+#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
+
 #define BTA_DISABLE_DELAY 1000
 
 #endif
