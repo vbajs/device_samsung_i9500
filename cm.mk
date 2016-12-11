f5a6a06dc3e8d6987962caa799eccb00f5d2d575
dragonGR <alextsanisbadlorg@gmail.com>
2016-12-11 01:20:39 +0000

    i9500: Include CM common file
    Change-Id: Ic30c78c93cc402a40d57a472b76f593e7aee892b
    
    Conflicts:
    	cm.mk

--- cm.mk
+++ cm.mk
@@ -5,6 +5,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common.mk)
 
 # Gello
 PRODUCT_PACKAGES += \
