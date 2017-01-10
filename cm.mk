4896822dd81c4ddad464db01ba00ac281fc0c60c
Yahya Wessam <yahyawessam2002@gmail.com>
2017-01-10 22:53:46 +0300

    i9500: Some CM improvements

--- cm.mk
+++ cm.mk
@@ -1,14 +1,7 @@
 $(call inherit-product, device/samsung/i9500/full_i9500.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Gello
-PRODUCT_PACKAGES += \
-    Gello
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=GT-I9500 \
@@ -18,3 +11,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="samsung/ja3gxx/ja3g:5.0.1/LRX22C/I9500XXUHOL1:user/release-keys" \
     PRIVATE_BUILD_DESC="ja3gxx-user 5.0.1 LRX22C I9500XXUHOL1 release-keys"
 
+PRODUCT_NAME := cm_i9500
+PRODUCT_DEVICE := i9500
